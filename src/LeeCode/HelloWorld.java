8db62220a57ef5d059b2edafbce7c1d4e5b1eb59
lmf757300481 <757300481@qq.com>
2018-06-14 09:28:27 +0800

    试一下

--- src/LeeCode/HelloWorld.java
+++ src/LeeCode/HelloWorld.java
@@ -1,9 +1,11 @@
+package LeeCode;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ajshfj");
+		System.out.println("这是LeeCode的HelloWorld");
+		System.out.println("这是LeeCode的HelloWorld");
 	}
 
 }
